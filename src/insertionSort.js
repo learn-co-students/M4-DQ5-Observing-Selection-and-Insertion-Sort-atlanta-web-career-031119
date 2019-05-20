76e99424cfb76a1fb7b71c773a65841262a66b41
Darcy Linde <lindedarcy@gmail.com>
2019-05-20 11:08:58 -0400

    insertion sort not working yet

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,16 @@
+function insert(array, rightIndex, value) {
+    for(let i = rightIndex;
+        i >= 0 && array[i] > value;
+        i--) {
+        array[i + 1] = array[i];
+    }
+    array[i + 1] = value;
+};
+
 function insertionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  const newArr = []
+  for(let i = 0; i < arr.length; i++){
+    insert(newArr,i,arr[i])
+  }
+  return newArr
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,27 @@
+function swap(array, firstIndex, secondIndex) {
+    let temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+function indexOfMinimum(array, startIndex) {
+    let minValue = array[startIndex];
+    let minIndex = startIndex;
+
+    for(let i = minIndex + 1; i < array.length; i++) {
+        if(array[i] < minValue) {
+            minIndex = i;
+            minValue = array[i];
+        }
+    }
+    return minIndex;
+};
+
+
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for(let i=0; i < arr.length; i++){
+    let index = indexOfMinimum(arr, i)
+    swap(arr, i, index)
+  }
+  return arr
 }
